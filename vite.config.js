cf27d1b5bd1062a1d47ad6fafce5d7bc6e5793ef
dengagaga <134398955+dengagaga@users.noreply.github.com>
2024-02-05 12:10:34 +0300

    four commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/vue-proj1",
+  base:"/",
   plugins: [
     vue(),
     vueJsx(),
